c30441a4749f23a6b6e2b9d926e24b1e6b801468
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-06-06 05:59:35 +0530

    adding header

---
+++ project/src/components/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-[#2699fb] p-4">
+      <div className="max-w-[1240px] border border-black mx-auto">
+        <div>New Tech</div>
+        <ul>
+          <li>Home</li>
+          <li>Company</li>
+          <li>Resources</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Header;
